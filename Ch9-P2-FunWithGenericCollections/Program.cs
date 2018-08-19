bf7efb879d125f7331fffeab788777b1962bec2a
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-08-20 02:10:15 +0500

    commit 1 from hostel

---
+++ Ch9-P2-FunWithGenericCollections/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch9_P2_FunWithGenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
